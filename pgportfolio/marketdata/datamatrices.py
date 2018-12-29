ff0ecb88691dca6369ba23ceddce02993fc7246d
Alexis Manchon <amanchon@starclay.fr>
2018-12-29 17:19:32 +0100

    live test

--- pgportfolio/marketdata/datamatrices.py
+++ pgportfolio/marketdata/datamatrices.py
@@ -153,7 +153,7 @@ class DataMatrices:
         return self.__pack_samples(self._train_ind[:-self._window_size])
 
     def get_live_set(self):
-        return self.__pack_samples(np.arange(self._num_periods))
+        return self.__pack_samples(np.arange(self._num_periods)[:-self._window_size])
 
     def next_batch(self):
         """
