22db1a004c7b7bd304073a0b3d4576030891d75d
Alexis Manchon <amanchon@starclay.fr>
2018-12-29 23:11:01 +0100

    live test

--- pgportfolio/marketdata/globaldatamatrix.py
+++ pgportfolio/marketdata/globaldatamatrix.py
@@ -2,6 +2,9 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 from pgportfolio.marketdata.coinlist import CoinList
 import numpy as np
 import pandas as pd
